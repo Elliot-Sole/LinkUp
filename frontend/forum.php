aad8e318da7a4b60d9292ee866c0911f81afdcb8
Elliot Sole <230111497@aston.ac.uk>
2026-02-07 21:11:20 +0000

    Update forum.php

--- frontend/forum.php
+++ frontend/forum.php
@@ -21,16 +21,31 @@
         </nav>
         </div>
         <h2>Forum</h2>
-        <form>
+        <form method = "POST" action = "forum.php">
             <input type = "text" id = "query">
             <input type = "submit" class = "btn-1" value = "Search">
         </form>
+        <?php
+                                if(ISSET($_POST['query'])){
+                                        $keyword = $_POST['keyword'];
+                        ?>
         <p>Here you can post about and sign up to community based events in your local area.</p>
         <a href = "newpost.php">
         <button class = "btn-1">New Post</button>
         </a>
         <section class = "posts">
             <h2>Latest Posts</h2>
+            <?php
+                                    $safe_value = mysql_real_escape_string($_POST['query']);
+
+                $result = mysql_query("SELECT CommentText FROM comments WHERE `BoardID` LIKE %$safe_value%");
+                 while ($row = mysql_fetch_assoc($result)) {
+                echo "<div id='link' onClick='addText(\"".$row['username']."\");'>" . $row['username'] . "</div>";  
+ }
+
+
+  ?>
+            
             <div class = "post-container">
 
             </div>
